51f4f6702600e5f29580a2ce6b9da24d3fd42024
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2026-04-10 18:02:41 +0100

    omv: default v13 + network fix + omv-extras installation step

--- install/omv-install.sh
+++ install/omv-install.sh
@@ -13,19 +13,39 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Installing OpenMediaVault (Patience)"
+msg_info "Installing OpenMediaVault (patience)"
 curl -fsSL "https://packages.openmediavault.org/public/archive.key" | gpg --dearmor >"/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.gpg"
-cat <<EOF >/etc/apt/sources.list.d/openmediavault.list
-deb [signed-by=/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm main
-EOF
-
-export LANG=C.UTF-8
-export DEBIAN_FRONTEND=noninteractive
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public synchrony main" >/etc/apt/sources.list.d/openmediavault.list
 export APT_LISTCHANGES_FRONTEND=none
-$STD apt update
-apt -y --auto-remove --show-upgraded --allow-downgrades --allow-change-held-packages --no-install-recommends --option DPkg::Options::="--force-confdef" --option DPkg::Options::="--force-confold" install openmediavault-keyring openmediavault &>/dev/null
+export DEBIAN_FRONTEND=noninteractive
+export LANG=C.UTF-8
+$STD apt-get update
+$STD apt-get install -y openmediavault openmediavault-keyring \
+  --allow-change-held-packages \
+  --allow-downgrades \
+  --auto-remove \
+  --no-install-recommends \
+  --option DPkg::Options::="--force-confdef" \
+  --option DPkg::Options::="--force-confold" \
+  --show-upgraded
 omv-confdbadm populate &>/dev/null
+(systemctl restart networking &) &>/dev/null
 msg_ok "Installed OpenMediaVault"
+
+msg_info "Regenerating OpenMediaVault index (patience)"
+timeout 300s bash -c "until ping -c1 -W1 community-scripts.org &>/dev/null; do sleep 1; done"
+$STD apt-get update
+msg_ok "Regenerated OpenMediaVault index"
+
+if whiptail \
+  --title "Customize OMV" \
+  --yesno "Would you like to add OMV-extras, plugins repository for OMV?\n(https://wiki.omv-extras.org/)" \
+  --defaultno \
+  8 60; then
+  msg_info "Installing OMV extras"
+  $STD bash <(curl -fsSL https://github.com/OpenMediaVault-Plugin-Developers/packages/raw/master/install)
+  msg_ok "Installed OMV extras"
+fi
 
 motd_ssh
 customize
