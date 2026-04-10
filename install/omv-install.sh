80090e37a844890e17a8490699a98546cd368ccb
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2026-04-10 18:02:41 +0100

    omv: default v13 + network fix + omv-extras installation step

--- install/omv-install.sh
+++ install/omv-install.sh
@@ -13,19 +13,47 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Installing OpenMediaVault (Patience)"
+msg_info "Installing OpenMediaVault (patience)"
+nameservers=$(grep '^nameserver' /etc/resolv.conf | awk '{print $2}' | xargs)
 curl -fsSL "https://packages.openmediavault.org/public/archive.key" | gpg --dearmor >"/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.gpg"
-cat <<EOF >/etc/apt/sources.list.d/openmediavault.list
-deb [signed-by=/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm main
-EOF
-
-export LANG=C.UTF-8
-export DEBIAN_FRONTEND=noninteractive
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public synchrony main" >/etc/apt/sources.list.d/openmediavault.list
 export APT_LISTCHANGES_FRONTEND=none
+export DEBIAN_FRONTEND=noninteractive
+export LANG=C.UTF-8
 $STD apt update
-apt -y --auto-remove --show-upgraded --allow-downgrades --allow-change-held-packages --no-install-recommends --option DPkg::Options::="--force-confdef" --option DPkg::Options::="--force-confold" install openmediavault-keyring openmediavault &>/dev/null
-omv-confdbadm populate &>/dev/null
+$STD apt install -y openmediavault openmediavault-keyring \
+  --allow-change-held-packages \
+  --allow-downgrades \
+  --auto-remove \
+  --no-install-recommends
 msg_ok "Installed OpenMediaVault"
+
+msg_info "Configuring OpenMediaVault (patience)"
+$STD omv-confdbadm populate
+sed -i "s/^#\?DNS=.*/DNS=$nameservers/" /etc/systemd/resolved.conf
+$STD systemctl restart systemd-resolved
+$STD apt update
+msg_ok "Configured OpenMediaVault index"
+
+echo
+read -r -p "${TAB3}Would you like to add plugins from OMV-extras repository? [y/N]: " CONFIRM
+echo
+if [[ "$CONFIRM" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  msg_warn "WARNING: This script will run an external installer from a third-party source (https://wiki.omv-extras.org/)."
+  msg_warn "The following code is NOT maintained or audited by our repository."
+  msg_warn "If you have any doubts or concerns, please review the installer code before proceeding:"
+  msg_custom "${TAB3}${GATEWAY}${BGN}${CL}" "\e[1;34m" "→  https://github.com/OpenMediaVault-Plugin-Developers/packages/raw/master/install"
+  echo
+  read -r -p "${TAB3}Do you want to continue? [y/N]: " CONFIRM
+  echo
+  if [[ "$CONFIRM" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    msg_info "Installing OMV extras"
+    $STD bash <(curl -fsSL https://github.com/OpenMediaVault-Plugin-Developers/packages/raw/master/install)
+    msg_ok "Installed OMV extras"
+  else
+    msg_error "Aborted by user. No changes have been made."
+  fi
+fi
 
 motd_ssh
 customize
