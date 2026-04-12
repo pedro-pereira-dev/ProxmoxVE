ffee3eaf8646c678e217a6513ed899ed971ab091
Pedro Pereira <11753516+pedro-pereira-dev@users.noreply.github.com>
2026-04-12 19:46:19 +0100

    chore: sets up development environment

--- install/docmost-install.sh
+++ install/docmost-install.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: MickLesk (Canbiz)
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/pedro-pereira-dev/ProxmoxVE/raw/main/LICENSE
 # Source: https://docmost.com/ | Github: https://github.com/docmost/docmost
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
